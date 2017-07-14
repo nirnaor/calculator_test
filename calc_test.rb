5bff8a7fc2989c97b17fef59d73b82b2d8787d38
nirnaor <nirnaori@gmail.com>
2017-07-14 22:28:18 +0300

    Adding calculator tests.

---
+++ calc_test.rb
@@ -0,0 +1,35 @@
+require 'test/unit'
+require_relative 'calculator'
+
+class TC_MyTest < Test::Unit::TestCase
+
+  def setup
+    @calc = Calculator.new
+  end
+
+  def test_plus
+    assert_equal(@calc.plus(2, 3), 5)
+  end
+
+  def test_plus
+    assert_equal(@calc.plus(3, 3), 6)
+  end
+
+  def test_sub
+    assert_equal(@calc.substract(3, 2), 1)
+  end
+
+  def test_divide
+    assert_equal(@calc.divide(100, 25), 4)
+  end
+
+  def test_divide2
+    assert_equal(@calc.divide(100, 0), 'cannot divide by zero')
+  end
+
+  def test_multiply
+    assert_equal(@calc.multiply(100, 2), 200)
+  end
+
+
+end
